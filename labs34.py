83dd8ef70810b68c5b8640db6659fb676f5b4bbb
vinupooji <43032019+vinupooji@users.noreply.github.com>
2018-09-20 16:20:26 +0530

    Add files via upload

---
+++ labs34.py
@@ -0,0 +1,13 @@
+a=int(input("enter the marks"))
+if a>=90:
+	print("A+")
+elif a>=80:
+	print("A")
+elif a>=70:
+	print("B+")
+elif a>=60:
+	print("B")
+elif a>=40:
+	print("C")
+else:
+	print("fail")			
