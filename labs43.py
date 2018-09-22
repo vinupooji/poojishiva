14b775021b526ed683f61db6c1fc8e78ad285655
vinupooji <43032019+vinupooji@users.noreply.github.com>
2018-09-22 09:31:07 +0530

    Add files via upload

---
+++ labs43.py
@@ -0,0 +1,4 @@
+a=5
+while((a<=5) and(a>=1)):
+	print(a)
+	a=a-1
---
+++ labs44.py
@@ -0,0 +1,8 @@
+a=0
+while a<=250:
+	print(a)
+	a=a+1	
+	if(a%2==0):
+		print(a,"is an even number")
+	else:
+		print(a,"is an odd number")	
